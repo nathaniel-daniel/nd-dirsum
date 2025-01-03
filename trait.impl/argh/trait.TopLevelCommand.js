51ed56a5de2f39011a960d074b384b302281a7e0
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-01-03 02:23:09 +0000

    deploy: 001b2729c825bb70d40e9014bf1bbd675199d560

---
+++ trait.impl/argh/trait.TopLevelCommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_dirsum",[["impl TopLevelCommand for <a class=\"struct\" href=\"nd_dirsum/struct.Options.html\" title=\"struct nd_dirsum::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[151]}
