f50fcf3c12ef9da1a092e77f9929f6885daf99c2
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-03-03 02:46:13 +0000

    deploy: c0527a325f4a72cbeaf8284da673b5953aa1228a

---
+++ search.desc/nd_dirsum/nd_dirsum-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("nd_dirsum", 0, "Returns the argument unchanged.\nCalls <code>U::from(self)</code>.")
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["nd_dirsum",["",[],["main.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[33]}
