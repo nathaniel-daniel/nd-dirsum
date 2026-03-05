cfa7a5eab5e54ef7364011f38e3900fc48f6591f
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2026-03-05 05:34:47 +0000

    deploy: 0edebbb4aea002eee818c460f731b3223e84abaa

---
+++ search.index/crateNames/8c196be22afd.js
@@ -0,0 +1,1 @@
+rd_("ind_dirsum")
---
+++ trait.impl/argh/trait.FromArgs.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nd_dirsum",[["impl FromArgs for <a class=\"struct\" href=\"nd_dirsum/struct.Options.html\" title=\"struct nd_dirsum::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[144]}
